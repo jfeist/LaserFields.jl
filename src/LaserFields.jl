3110b1891d2e33f606129a2e585ab58bbc04e88b
Johannes Feist <johannes.feist@gmail.com>
2022-10-24 11:40:33 +0200

    update atomic unit values with CODATA2018 data (from Unitful.jl)

--- src/LaserFields.jl
+++ src/LaserFields.jl
@@ -7,16 +7,16 @@ using DataInterpolations
 export make_laser_field, E_field, A_field, E_fourier, A_fourier, start_time, end_time
 
 const GAUSSIAN_TIME_CUTOFF_SIGMA = 3.5*sqrt(log(256))
-const au_as   = 1/24.188843265     # attosecond in a.u.
-const au_wcm2toel2 = 1/3.509338e16 # W/cm^2 in a.u. for electric field squared
-const au_wcm2 = 1.55369e-16        # W/cm^2 in a.u
-const au_m    = 1/5.291772098e-11  # m in a.u.
-const au_cm   = 1/5.291772098e-9   # cm in a.u.
-const au_nm   = 1/5.291772098e-2   # nm in a.u.
-const au_c    = 137.03599911       # c (speed of light) in a.u. == 1/alpha
-const au_eV   = 1/27.2113845       # eV in a.u.
-const au_m_He = 7294.2995365       # m of He-nucleus in a.u.
-const au_m_n  = 1838.6836605       # m of neutron in a.u.
+const au_as   = 1/24.188843265903884 # attosecond in a.u.
+const au_wcm2toel2 = 1/3.5094455205784296e16 # W/cm^2 in a.u. for electric field squared
+const au_wcm2 = 1.5536611487396207e-16 # W/cm^2 in a.u
+const au_m    = 1/5.29177210903e-11  # m in a.u.
+const au_cm   = 1/5.29177210903e-9   # cm in a.u.
+const au_nm   = 1/5.29177210903e-2   # nm in a.u.
+const au_c    = 137.0359990836958    # c (speed of light) in a.u. == 1/alpha
+const au_eV   = 1/27.211386245935508 # eV in a.u.
+const au_m_He = 7294.299386612553    # m of He nucleus in a.u.
+const au_m_n  = 1838.6836617324586   # m of neutron in a.u.
 
 abstract type LaserField end
 Base.Broadcast.broadcastable(lf::LaserField) = Ref(lf)
