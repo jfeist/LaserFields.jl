9a565213d5f4af0158adf08bb4b334803ae8bff4
Johannes Feist <johannes.feist@gmail.com>
2022-10-29 23:27:59 +0200

    some notation changes and add tests

--- src/LaserFields.jl
+++ src/LaserFields.jl
@@ -21,11 +21,11 @@ const au_m_n  = 1838.6836617324586   # m of neutron in a.u.
 abstract type LaserField end
 Base.Broadcast.broadcastable(lf::LaserField) = Ref(lf)
 
-macro _standard_props()
+macro _standard_lf_props()
     esc(quote
             is_vecpot::Bool
             E0::T1
-            omega::T2
+            ω0::T2
             peak_time::T3
             duration::T4
             chirp::T5
@@ -34,39 +34,39 @@ macro _standard_props()
 end
 
 Base.@kwdef struct GaussianLaserField{T1,T2,T3,T4,T5,T6} <: LaserField
-    @_standard_props
+    @_standard_lf_props
 end
 
 Base.@kwdef struct SinExpLaserField{T1,T2,T3,T4,T5,T6,T7} <: LaserField
-    @_standard_props
+    @_standard_lf_props
     exponent::T7
 end
 
 abstract type FlatTopLaserField <: LaserField end
 
 Base.@kwdef struct LinearFlatTopLaserField{T1,T2,T3,T4,T5,T6,T7} <: FlatTopLaserField
-    @_standard_props
+    @_standard_lf_props
     rampon::T7
 end
 
 Base.@kwdef struct Linear2FlatTopLaserField{T1,T2,T3,T4,T5,T6,T7} <: FlatTopLaserField
-    @_standard_props
+    @_standard_lf_props
     rampon::T7
 end
 
-TX(lf::LaserField) = 2π/lf.omega
-get_omega(lf::LaserField,t) = lf.omega + lf.chirp*(t-lf.peak_time)
+TX(lf::LaserField) = 2π/lf.ω0
+get_ωt(lf::LaserField,t) = lf.ω0 + lf.chirp*(t-lf.peak_time)
 
 (lf::LaserField)(t) = lf.is_vecpot ? A_field(lf,t) : E_field(lf,t)
 
 function E_field(lf::LaserField,t)
     env, envpr = envelope(lf,t)
-    omega = get_omega(lf,t)
-    osc   = sin(omega*(t-lf.peak_time) + π*lf.phase_pi)
+    ω = get_ωt(lf,t)
+    osc   = sin(ω*(t-lf.peak_time) + π*lf.phase_pi)
     if lf.is_vecpot
-        # d(w(t)*(t-peak))/dt = w(t) + w'(t)*(t-peak) = omega + lf%omega * lf%chirp * (t-peak) = 2 * omega - lf%omega
-        oscpr = (2*omega-lf.omega)*cos(omega*(t-lf.peak_time)+π*lf.phase_pi)
-        return -(env * oscpr + envpr * osc) / lf.omega
+        # d(w(t)*(t-peak))/dt = w(t) + w'(t)*(t-peak) = ω + lf.ω0 * lf.chirp * (t-tpeak) = 2ω - lf.ω0
+        oscpr = (2ω-lf.ω0)*cos(ω*(t-lf.peak_time)+π*lf.phase_pi)
+        return -(env * oscpr + envpr * osc) / lf.ω0
     else  # describes electric field directly
         return env * osc
     end
@@ -76,69 +76,69 @@ function A_field(lf::LaserField,t)
     lf.is_vecpot || error("laser field is not given as a vector potential, cannot get A(t) analytically!")
 
     env, envpr = envelope(lf,t)
-    omega = get_omega(lf,t)
-    osc = sin(omega * (t-lf.peak_time) + π*lf.phase_pi)
+    ω = get_ωt(lf,t)
+    osc = sin(ω * (t-lf.peak_time) + π*lf.phase_pi)
     # Divide out derivative of oscillation to ensure peak amplitude of E0 for electric field
-    return env*osc / lf.omega
+    return env*osc / lf.ω0
 end
 
 """return the fourier transform of the envelope of the laser field.
 we write the whole pulse as
-f(t) = (env(t) exp(i*(phi0 + w0*tp + chirp*tp**2)) + c.c. ) / (2*IU), where tp = t-tpeak
+f(t) = (env(t) exp(i*(phi0 + ω0*tp + chirp*tp^2)) + c.c. ) / 2im, where tp = t-tpeak
 for the fourier transform of the envelope, we include the chirp term
-exp(i chirp (t-tpeak)**2) in the envelope, so that its fourier transform is a complex function.
+exp(i chirp (t-tpeak)^2) in the envelope, so that its fourier transform is a complex function.
 however, for unchirped pulses, the result will be purely real!
 
 for the various calculations, see chirped_fourier.nb in the mathematica directory."""
 function envelope_fourier end
 
-function E_fourier(lf::LaserField,omega)
+function E_fourier(lf::LaserField,ω)
     # analytically determine the fourier transform of the defined laser fields
-    # determined as Int exp(-i*omega*t) E(t) dt
+    # determined as Int exp(-i*ω*t) E(t) dt
 
     # with tp = t-tpeak, the whole pulse is
-    # f(t) =  env(t) sin    (phi0 + w0*tp + chirp*tp**2)
-    #      = (env(t) exp(IU*(phi0 + w0*tp + chirp*tp**2)) - c.c. ) / (2*IU)
-    # for the fourier transform, we include the chirp term exp(i chirp tp**2) in the envelope.
+    # f(t) =  env(t) sin    (phi0 + ω0*tp + chirp*tp^2)
+    #      = (env(t) exp(IU*(phi0 + ω0*tp + chirp*tp^2)) - c.c. ) / 2im
+    # for the fourier transform, we include the chirp term exp(i chirp tp^2) in the envelope.
     # this part is transformed in lf_envelope_fourier.
-    # exp(IU*phi0) is just a constant prefactor, and the linear phase w0*tp just gives a shift in frequency,
-    # F[f(t) exp(IU w0 t)](w) = F[f(t)](w-w0)
+    # exp(IU*phi0) is just a constant prefactor, and the linear phase ω0*tp just gives a shift in frequency,
+    # F[f(t) exp(im ω0 t)](ω) = F[f(t)](ω-ω0)
     # complex conjugation of the transformed function gives complex conjugation + reversal of the argument in the transform, so
-    # F[conjg(f(t) exp(IU w0 t))](w) = conjg(F[f(t) exp(IU w0 t)](-w)) = conjg(F[f(t)](-w-w0))
-    ELFT = (   envelope_fourier(lf, omega - lf.omega) * cispi(lf.phase_pi)
-            - (envelope_fourier(lf,-omega - lf.omega) * cispi(lf.phase_pi))') / 2im
+    # F[conjg(f(t) exp(im ω0 t))](ω) = conjg(F[f(t) exp(IU ω0 t)](-ω)) = conjg(F[f(t)](-ω-ω0))
+    ELFT = (   envelope_fourier(lf, ω-lf.ω0) * cispi(lf.phase_pi)
+            - (envelope_fourier(lf,-ω-lf.ω0) * cispi(lf.phase_pi))') / 2im
 
     # the fourier transform of the part was determined as if it was centered around t=0
-    # shift in time now -- just adds a phase exp(-IU*omega*peak_time), as F[f(t-a)] = exp(-IU*omega*a) F[f(t)]
-    ELFT *= cis(-omega*lf.peak_time)
+    # shift in time now -- just adds a phase exp(-im*ω*peak_time), as F[f(t-a)] = exp(-im*ω*a) F[f(t)]
+    ELFT *= cis(-ω*lf.peak_time)
 
     if lf.is_vecpot
         # if this laser field was defined as a vector potential, we need to multiply
-        # with -IU*omega to get the fourier transform of the electric field, E=-dA/dt
-        # F[-dA/dt] = -iw F[A]
-        # in addition, we need to take into account that A0 = E0 / lf%omega
-        ELFT *= -1im * omega / lf.omega
+        # with -im*ω to get the fourier transform of the electric field, E=-dA/dt
+        # F[-dA/dt] = -iω F[A]
+        # in addition, we need to take into account that A0 = E0 / lf.ω0
+        ELFT *= -1im * ω / lf.ω0
     end
     return ELFT
 end
 
-A_fourier(lf::LaserField,omega) = E_fourier(lf,omega) / (-1im*omega)
+A_fourier(lf::LaserField,ω) = E_fourier(lf,ω) / (-1im*ω)
 
 function envelope(lf::GaussianLaserField,t)
     env   = lf.E0 * exp(-(t-lf.peak_time)^2/(2*lf.duration^2))
     envpr = env * (lf.peak_time-t)/lf.duration^2
     return env,envpr
 end
-function envelope_fourier(lf::GaussianLaserField,omega)
+function envelope_fourier(lf::GaussianLaserField,ω)
     # F[exp(-z*t^2)] = exp(-w^2/4z)/sqrt(2z) (for real(z)>0)
     z = 0.5/lf.duration^2 - 1im*lf.chirp
-    return lf.E0 * exp(-omega^2/4z) / sqrt(2z)
+    return lf.E0 * exp(-ω^2/4z) / sqrt(2z)
 end
 start_time(lf::GaussianLaserField) = lf.peak_time - GAUSSIAN_TIME_CUTOFF_SIGMA*lf.duration
 end_time(  lf::GaussianLaserField) = lf.peak_time + GAUSSIAN_TIME_CUTOFF_SIGMA*lf.duration
 
 function expiatbt2_intT(a,b,T)
-    # returns the result of the integral Int(exp(i*(a*t+b*t**2)),{t,-T/2,T/2}) / sqrt(2*pi)
+    # returns the result of the integral Int(exp(i*(a*t+b*t**2)),{t,-T/2,T/2}) / sqrt(2π)
     zz1 = (1+1im)/4
     z34 = (-1+1im)/sqrt(2) # == (-1)**(3/4)
     cb = complex(b) # we want to take the square root and b might be negative
@@ -161,43 +161,43 @@ function envelope(lf::SinExpLaserField,t)
     return env,envpr
 end
 
-function envelope_fourier(lf::SinExpLaserField,omega)
+function envelope_fourier(lf::SinExpLaserField,ω)
     if lf.exponent == 2
         if lf.chirp == 0
             # the expression with chirp can not be evaluated with chirp == 0, so we take this as a special case
-            return lf.E0 * sqrt(8π^3) * sinc(omega*lf.duration/2π)/(8π^2/lf.duration - 2*omega^2*lf.duration)
+            return lf.E0 * sqrt(8π^3) * sinc(ω*lf.duration/2π)/(8π^2/lf.duration - 2*ω^2*lf.duration)
         else
             # now we use that cos(pi*t/T)**2 * exp(i*c*t**2) can be written as 0.5 exp(i*c*t**2) + 0.25 exp(i*c*t**2 - 2*i*pi*t/T) + 0.25 exp(i*c*t**2 + 2*i*pi*t/T)
             # the integral of exp(IU*(a*t+b*t**2)) from t=-T/2 to t=T/2 can be calculated analytically and is implemented in the function below
-            # the arguments are a={-omega, -2*pi/T-omega, 2*pi/T-omega} and b=chirp
+            # the arguments are a={-ω, -2*pi/T-ω, 2*pi/T-ω} and b=chirp
             wd = 2π/lf.duration
-            return lf.E0 * (expiatbt2_intT(    - omega, lf.chirp, lf.duration)/2 +
-                            expiatbt2_intT(-wd - omega, lf.chirp, lf.duration)/4 +
-                            expiatbt2_intT( wd - omega, lf.chirp, lf.duration)/4)
+            return lf.E0 * (expiatbt2_intT(    - ω, lf.chirp, lf.duration)/2 +
+                            expiatbt2_intT(-wd - ω, lf.chirp, lf.duration)/4 +
+                            expiatbt2_intT( wd - ω, lf.chirp, lf.duration)/4)
         end
     elseif lf.exponent == 4
         if lf.chirp == 0
             # the expression with chirp can not be evaluated with chirp == 0, so we take this as a special case
-            return lf.E0 * 24 * (sqrt(2π^7) * sinc(omega*lf.duration/2π) /
-                                    (128π^4/lf.duration - 40π^2*omega^2*lf.duration + 2omega^4*lf.duration^3))
+            return lf.E0 * 24 * (sqrt(2π^7) * sinc(ω*lf.duration/2π) /
+                                    (128π^4/lf.duration - 40π^2*ω^2*lf.duration + 2ω^4*lf.duration^3))
         else
             # now we use that cos(pi*t/T)**4 * exp(i*c*t**2) can be written as
             # (0.375 exp(i*c*t**2) + 0.25 exp(i*c*t**2 - 2*i*pi*t/T) + 0.25 exp(i*c*t**2 + 2*i*pi*t/T) +
             #  0.0625 exp(i*c*t**2 - 4*i*pi*t/T) + 0.0625 exp(i*c*t**2 + 4*i*pi*t/T))
             # the integral of exp(IU*(a*t+b*t**2)) from t=-T/2 to t=T/2 can be calculated analytically and is implemented in the function below
-            # the arguments are a={-omega, -2*pi/T-omega, 2*pi/T-omega, -4*pi/T-omega, 4*pi/T-omega} and b=chirp
+            # the arguments are a={-ω, -2*pi/T-ω, 2*pi/T-ω, -4*pi/T-ω, 4*pi/T-ω} and b=chirp
             wd = 2π/lf.duration
-            return lf.E0 * (expiatbt2_intT(      - omega, lf.chirp, lf.duration)*0.375  +
-                            expiatbt2_intT(  -wd - omega, lf.chirp, lf.duration)*0.25   +
-                            expiatbt2_intT(   wd - omega, lf.chirp, lf.duration)*0.25   +
-                            expiatbt2_intT(-2*wd - omega, lf.chirp, lf.duration)*0.0625 +
-                            expiatbt2_intT( 2*wd - omega, lf.chirp, lf.duration)*0.0625)
+            return lf.E0 * (expiatbt2_intT(     - ω, lf.chirp, lf.duration)*0.375  +
+                            expiatbt2_intT( -wd - ω, lf.chirp, lf.duration)*0.25   +
+                            expiatbt2_intT(  wd - ω, lf.chirp, lf.duration)*0.25   +
+                            expiatbt2_intT(-2wd - ω, lf.chirp, lf.duration)*0.0625 +
+                            expiatbt2_intT( 2wd - ω, lf.chirp, lf.duration)*0.0625)
         end
     else
         if lf.chirp != 0 || !isinteger(lf.exponent)
             error("sin_exp fourier transform with exponent != 2 or 4 only implemented for integer exponents and unchirped pulses")
         end
-        x = 0.5*(omega*lf.duration/π - lf.exponent)
+        x = 0.5*(ω*lf.duration/π - lf.exponent)
         return lf.E0 * lf.duration * gamma(lf.exponent+1)*gamma(x)*sinpi(x)/(sqrt(2^(2lf.exponent+1) * π^3) * gamma(x+lf.exponent+1))
     end
 end
@@ -238,18 +238,18 @@ ramponfuncpr(lf::LinearFlatTopLaserField,trel) = 1.
 ramponfunc(lf::Linear2FlatTopLaserField,trel) = sin(π/2*trel)^2
 ramponfuncpr(lf::Linear2FlatTopLaserField,trel) = sin(π*trel) * π/2
 
-function envelope_fourier(lf::LinearFlatTopLaserField,omega)
+function envelope_fourier(lf::LinearFlatTopLaserField,ω)
     lf.chirp == 0 || error("Fourier transform of 'linear' field with chirp not implemented!")
-    return lf.E0 * sqrt(8/π) * sinc(omega*lf.rampon/2π) * sinc(omega*(lf.rampon+lf.duration)/2π) * (lf.rampon+lf.duration)/4
+    return lf.E0 * sqrt(8/π) * sinc(ω*lf.rampon/2π) * sinc(ω*(lf.rampon+lf.duration)/2π) * (lf.rampon+lf.duration)/4
 end
 
-function envelope_fourier(lf::Linear2FlatTopLaserField,omega)
+function envelope_fourier(lf::Linear2FlatTopLaserField,ω)
     lf.chirp == 0 || error("Fourier transform of 'linear2' field with chirp not implemented!")
-    return lf.E0 * sqrt(2π^3) * cos(omega*lf.rampon/2) * sinc(omega*(lf.rampon+lf.duration)/2π) * (lf.rampon+lf.duration)/ (2π^2 - 2*lf.rampon^2*omega^2)
+    return lf.E0 * sqrt(2π^3) * cos(ω*lf.rampon/2) * sinc(ω*(lf.rampon+lf.duration)/2π) * (lf.rampon+lf.duration)/ (2π^2 - 2*lf.rampon^2*ω^2)
 end
 
-struct InterpolatingLaserField{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10} <: LaserField
-    @_standard_props
+Base.@kwdef struct InterpolatingLaserField{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10} <: LaserField
+    @_standard_lf_props
     datafile::String
     Efun::T7
     Afun::T8
@@ -257,7 +257,7 @@ struct InterpolatingLaserField{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10} <: LaserField
     end_time::T10
 end
 
-function InterpolatingLaserField(; is_vecpot,datafile)
+function InterpolatingLaserField(datafile; is_vecpot)
     # print('# Reading laser_field from file:', datafile)
     data = readdlm(datafile)
     ndims(data) == 2 && size(data,2) == 2 || error("Laser field datafile '$datafile' must contain two columns: time and field")
@@ -266,41 +266,43 @@ function InterpolatingLaserField(; is_vecpot,datafile)
     # print('# Number of data points found:', len(tt))
     issorted(tt) || error("Laser field datafile '$datafile' must be sorted by time")
 
+    # analyze the data we have read to guess some information about the field
+    start_time = tt[1]
+    end_time = tt[end]
+    
     if is_vecpot
         Afun = CubicSpline(ff,tt)
         Efun = t -> -DataInterpolations.derivative(Afun,t)
     else
         Efun = CubicSpline(ff,tt)
-        Afun = t -> -DataInterpolations.integral(Efun,tt[1],t)
+        Afun = t -> -DataInterpolations.integral(Efun,start_time,t)
     end
-
-    # analyze the data we have read to guess some information about the field
-    start_time = tt[1]
-    end_time = tt[end]
 
     TX = Inf
     E0 = 0.
-    Eprev = 0.
+    Eprev = Efun(tt[1])
     peak_time = 0.
-    lastzerocrossing = start_time
+    lastzerocrossing = -Inf
     for t in LinRange(tt[1],tt[end],20*length(tt))
         E = Efun(t)
         if abs(E) > E0
             E0 = abs(E)
             peak_time = t
         end
-        if E*Eprev < 0
+        if sign(E) != sign(Eprev)
             TX = min(TX,2*(t-lastzerocrossing))
             lastzerocrossing = t
+            # @show t, lastzerocrossing, TX
+            Eprev = E
         end
     end
 
-    omega = 2π / TX
+    ω0 = 2π / TX
     duration = tt[end] - tt[1]
     chirp = 0.
     phase_pi = 0.
 
-    InterpolatingLaserField(is_vecpot,E0,omega,peak_time,duration,chirp,phase_pi,datafile,Efun,Afun,start_time,end_time)
+    InterpolatingLaserField(; is_vecpot,E0,ω0,peak_time,duration,chirp,phase_pi,datafile,Efun,Afun,start_time,end_time)
 end
 
 start_time(lf::InterpolatingLaserField) = lf.start_time
@@ -360,7 +362,7 @@ function make_laser_field(; form::Symbol, is_vecpot, phase_pi=0, pargs...)
     else
         0
     end
-    kwargs = Dict(pairs((is_vecpot=is_vecpot, phase_pi=phase_pi, E0=E0, omega=omega,
+    kwargs = Dict(pairs((is_vecpot=is_vecpot, phase_pi=phase_pi, E0=E0, ω0=omega,
                          peak_time=peak_time, duration=duration, chirp=chirp)))
     if   form in (:gaussian,:gaussianF)
         # convert from FWHM of field to standard deviation of field
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,59 @@
 using LaserFields
+using LaserFields: get_ωt
 using Test
 
 @testset "LaserFields.jl" begin
     # Write your tests here.
+    using LaserFields
+    using LaserFields: GaussianLaserField, SinExpLaserField, LinearFlatTopLaserField, Linear2FlatTopLaserField, InterpolatingLaserField
+
+    general_args = (is_vecpot=true,E0=1.5,ω0=0.12,peak_time=500.,chirp=0.,phase_pi=0.8)
+    test_fields = [
+        GaussianLaserField(;     general_args...,duration=100.),
+        SinExpLaserField(;       general_args...,duration=800.,exponent=2),
+        SinExpLaserField(;       general_args...,duration=800.,exponent=4),
+        SinExpLaserField(;       general_args...,duration=800.,exponent=7),
+        LinearFlatTopLaserField(;general_args...,duration=400.,rampon=150),
+        Linear2FlatTopLaserField(;general_args...,duration=400.,rampon=150),
+        InterpolatingLaserField("laserdat.dat", is_vecpot=true),
+        InterpolatingLaserField("laserdat.dat", is_vecpot=false)
+    ];
+
+    for lf in test_fields[begin:end-2]
+        @test lf.is_vecpot == true
+        @test lf.E0 == 1.5
+        @test lf.ω0 == 0.12
+        @test lf.peak_time == 500
+        @test lf.chirp == 0.0
+        @test lf.phase_pi == 0.8
+
+        @test get_ωt(lf, 0.)   == 0.12
+        @test get_ωt(lf, 500.) == 0.12
+        @test get_ωt(lf, 600.) == 0.12
+    end
+
+    let lf = test_fields[end-1]
+        @test lf.is_vecpot == true
+        @test lf.E0 == 0.1598555396981788
+        @test lf.ω0 == 0.16126036751849762
+        @test lf.peak_time == 353.36430391128425
+        @test lf.duration == 1000.0
+        @test lf.phase_pi == 0.0
+        @test lf.chirp == 0.0
+        @test lf.datafile == "laserdat.dat"
+        @test start_time(lf) == 0.0
+        @test end_time(lf) == 1000.0
+    end
+    let lf = test_fields[end]
+        @test lf.is_vecpot == false
+        @test lf.E0 == 0.9968278450466621
+        @test lf.ω0 == 0.16002937234660114
+        @test lf.peak_time == 343.6735101653429
+        @test lf.duration == 1000.0
+        @test lf.phase_pi == 0.0
+        @test lf.chirp == 0.0
+        @test lf.datafile == "laserdat.dat"
+        @test start_time(lf) == 0.0
+        @test end_time(lf) == 1000.0
+    end
 end
