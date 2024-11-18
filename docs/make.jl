ca832c040fe853057670f3790aa232004d360c42
Johannes Feist <johannes.feist@gmail.com>
2024-11-18 09:40:32 +0100

    update to current PkgTemplates defaults (github actions and docs)

--- docs/make.jl
+++ docs/make.jl
@@ -6,10 +6,8 @@ DocMeta.setdocmeta!(LaserFields, :DocTestSetup, :(using LaserFields); recursive=
 makedocs(;
     modules=[LaserFields],
     authors="Johannes Feist <johannes.feist@gmail.com> and contributors",
-    repo="https://github.com/jfeist/LaserFields.jl/blob/{commit}{path}#{line}",
     sitename="LaserFields.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://jfeist.github.io/LaserFields.jl",
         edit_link="main",
         assets=String[],
